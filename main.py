d2b473535599d153cc3d03a585984d62e72dd48d
Austin Brehm <austin.brehm@gmail.com>
2023-04-12 12:48:48 -0600

    clean up code

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# the goal of this program is to predict the price of coffee based on the size (oz)
-# use linear regression with stochastic gradient descent to find w,b that minimizes cost function
+# Goal: predict the price of coffee ($) based on size (oz).
+# Linear regression with stochastic gradient descent is used to find w,b values that minimize a cost function.
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-
-# data: x_train = size, y_train = price
-size = np.array([8, 12, 16, 20, 8, 8, 8, 8, 8, 12, 12, 12, 12, 12])
-price = np.array([1.50, 1.75, 2, 2.50, 1.55, 1.60, 1.45, 1.60, 1.58, 1.80, 1.70, 1.75, 1.72, 1.78])
 
 
 def plot(x, y, w, b, title, x_label, y_label):
@@ -63,40 +59,42 @@ def gradient_descent(x, y, w, b, alpha, iterations):
     return w, b, cost_history
 
 
-# initialize w, b, alpha (learning rate), and iterations
-# Note: change these parameters to alter model output
+# 1. Data Collection [Note: x_train = size, y_train = price]
+size = np.array([8, 12, 16, 20, 8, 8, 8, 8, 8, 12, 12, 12, 12, 12])
+price = np.array([1.50, 1.75, 2, 2.50, 1.55, 1.60, 1.45, 1.60, 1.58, 1.80, 1.70, 1.75, 1.72, 1.78])
+
+# 2. Initialize w, b, alpha (learning rate), and iterations [Note: change these parameters to alter model output]
 initial_w = 0
 initial_b = 0
 initial_alpha = 0.00005
 initial_iterations = 10000
 
-# plot initial data
-# plot(size, price, 'Initial Data: Coffee Prices', 'size (oz)', 'price ($)')
-
-# compute initial cost using initial parameters
+# 3. Compute initial cost using initial parameters
 initial_cost = compute_cost_function(size, price, initial_w, initial_b)
 print(f'Initial Cost Function Value: {initial_cost:0.2f}')
 
-# compute gradient
+# 4. Compute gradient
 w_gradient, b_gradient = compute_gradient(size, price, initial_w, initial_b)
 print(f'dj_dw: {w_gradient:0.2f}\ndj_db: {b_gradient:0.2f}')
 
-# compute w and b using gradient descent
+# 5. Compute w and b using gradient descent
 w_final, b_final, cost_history_final = gradient_descent(size, price, initial_w, initial_b, initial_alpha,
                                                         initial_iterations)
 print(f'w_final: {w_final:0.2f}\nb_final: {b_final:0.2f}')
 
+# 6. Compute final cost
 final_cost = compute_cost_function(size, price, w_final, b_final)
 print(f'Final Cost: {final_cost:0.2f}')
 
-# predict price for 10 oz coffee using w_final and b_final
-size_10 = w_final * 10 + b_final
-print(f'Price prediction for 10 oz: ${size_10:0.2f}')
+# 7. Predict price for 10 oz coffee using w_final and b_final
+predict_size_10 = w_final * 10 + b_final
+print(f'Price prediction for 10 oz: ${predict_size_10:0.2f}')
 
-if 1.50 < size_10 < 1.75:
+# 8. Test results
+if 1.50 < predict_size_10 < 1.75:
     print('SUCCESS')
 else:
     print('\nFAIL')
 
-# plot initial data and line
+# 9. Plot initial data and line of best fit
 plot(size, price, w_final, b_final, 'Linear Regression: Coffee Prices', 'size (oz)', 'price ($)')
